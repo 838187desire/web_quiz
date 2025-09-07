38577dad6b96cdaf39ea98550d922827e5acf2f8
838187desire <214587541+838187desire@users.noreply.github.com>
2025-09-07 02:18:10 -0700

    Update main.js

--- main.js
+++ main.js
@@ -2,12 +2,13 @@
 //   window.location.href = subject + '.html';
 // }
 
+function goToSubject(path) {
+  window.location.href = "/" + path + ".html";
+
+}
+
 // function goToSubject(path) {
-//   window.location.href = "/" + path + ".html";
+//   window.location.href =   path + ".html";
 
 // }
 
-function goToSubject(path) {
-  window.location.href =   path + ".html";
-
-}
