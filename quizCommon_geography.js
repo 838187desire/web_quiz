468f7a276c9d2b1916469ad63daa8ea8e6d99892
838187desire <214587541+838187desire@users.noreply.github.com>
2025-10-27 18:14:25 -0700

    Update quizCommon_geography.js

--- quizCommon_geography.js
+++ quizCommon_geography.js
@@ -30,23 +30,26 @@ function showQuestion() {
   let currentQuestion = questions[currentQuestionIndex];
   questionElement.innerHTML = `${currentQuestionIndex }. ${currentQuestion.question}`;
 
-
+ let btn_number = 1;
 
   currentQuestion.answers.forEach(answer => {
     const button = document.createElement("button");
-    button.innerText = answer.text;
+    button.innerText ="("+ btn_number+").       ";
+    button.innerText += "    "+answer.text;
     button.classList.add("btn");
     if (answer.correct) {
       button.dataset.correct = answer.correct;
     }
     button.addEventListener("click", selectAnswer);
     ansBtns.appendChild(button);
+    btn_number++;
   });
 
   explanationElement.innerHTML = `${currentQuestion.Explanation}`;
   explanationElement.style.display = "none";
 }
 
+ btn_number=0. 
 function resetState() {
   nextBtn.style.display = "none";
   ansBtns.innerHTML = "";
@@ -206,4 +209,5 @@ function createComplainBox() {
   quizContainer.appendChild(complainBox);
 }
 startQuiz();
+
 
